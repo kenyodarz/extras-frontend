b4ae0205aee40afd7fc9e1e8c207f2ab60f23ea1
Kenyodarz <minajorge@hotmail.com>
2020-04-27 11:18:16 -0500

    Version 0.0.0.16

--- src/app/components/entradas/entradas.component.ts
+++ src/app/components/entradas/entradas.component.ts
@@ -12,17 +12,21 @@ import {
   FormBuilder,
   FormGroup,
   FormControl,
-  Validators
+  Validators,
+  FormArray
 } from "@angular/forms";
 // PrimeNg
 import { MessageService, ConfirmationService } from "primeng/api";
+import { MenuItem } from "primeng/api";
 //Modelo
 import { Persona } from "src/app/models/Persona";
 import { Proyecto } from "src/app/models/Proyecto";
+import { Registro } from 'src/app/models/Registro';
 // Servicios
 import { PersonaService } from "src/app/services/persona.service";
 import { AuthService } from "src/app/services/auth.service";
 import { ProyectoService } from "src/app/services/proyecto.service";
+import { RegistroService } from "src/app/services/registro.service";
 
 @Component({
   selector: "app-entradas",
@@ -58,8 +62,10 @@ import { ProyectoService } from "src/app/services/proyecto.service";
 export class EntradasComponent implements OnInit {
   personas: Persona[];
   proyectos: Proyecto[];
+  registros: Registro[];
   selectedPersona: Persona;
   selectedProyecto: Proyecto;
+  selectedRegistro: Registro;
   display: boolean;
   es: any;
   date: Date;
@@ -71,10 +77,14 @@ export class EntradasComponent implements OnInit {
   form: any = {};
   errorMessage = "";
   public formEntrada: FormGroup;
+  timeValue: string;
+  cols: any[];
+  items: MenuItem[];
 
   constructor(
     private personaService: PersonaService,
     private proyectoService: ProyectoService,
+    private registroService: RegistroService,
     private messageService: MessageService,
     private confirmationService: ConfirmationService,
     private authService: AuthService,
@@ -83,14 +93,7 @@ export class EntradasComponent implements OnInit {
     this.display = true;
     this.personas = [];
     this.proyectos = [];
-    this.formEntrada = this.formBuilder.group({
-      persona: new FormControl(null, Validators.required),
-      proyecto: new FormControl(null, Validators.required),
-      hora_entrada: new FormControl(null, Validators.required),
-      hora_salida: new FormControl(null, Validators.required),
-      fecha: new FormControl(null, Validators.required),
-      festivo: new FormControl(false, Validators.required)
-    });
+    this.registros = [];
   }
 
   getAllPersona() {
@@ -125,6 +128,25 @@ export class EntradasComponent implements OnInit {
     );
   }
 
+  getAllRegistros() {
+    this.registroService.getAll().subscribe(
+      (result: any) => {
+        let registros: Registro[] = [];
+        for (let i = 0; i < result.length; i++) {
+          let registro = result[i] as Registro;
+          if (this.date == registro.fecha) {
+            registros.push(registro);
+          }
+        }
+        this.registros = registros;
+        console.log(this.registros);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
   onSubmit() {
     this.authService.register(this.form).subscribe(
       data => {
@@ -140,7 +162,31 @@ export class EntradasComponent implements OnInit {
   }
 
   agregarEntrada() {
-    console.log(this.formEntrada.value);
+    console.log(this.formEntrada.value + "Agregar Entrada");
+    console.log(JSON.stringify(this.formEntrada.value));
+    this.registroService.save(this.formEntrada.value).subscribe(
+      (result: any) => {
+        let registro = result as Registro;
+        //this.validarRegistro(registro);
+        this.getAllRegistros();
+        this.messageService.add({
+          severity: "success",
+          summary: "Resultado",
+          detail: "Se Agrego el Registro Correctamente"
+        });
+      },
+      error => {
+        console.log(error);
+        this.messageService.add({
+          severity: "warn",
+          summary: "¡¡¡Advertencia!!!",
+          detail: "No ha Seleccionado ningun Registro"
+        });
+      }
+    );
+  }
+  validarRegistro(registro: Registro) {
+    throw new Error("Method not implemented.");
   }
 
   get persona() {
@@ -167,20 +213,29 @@ export class EntradasComponent implements OnInit {
     return this.formEntrada.get("festivo");
   }
 
-  onPersonaChange() {
-    let persona: Persona = this.persona.value;
-    console.log("Persona Seleccionada: " + persona.nombre);
+  onFechaChange() {
+    console.log("Fecha Seleccionada: " + this.date);
+    this.getAllRegistros();
   }
 
   aceptar() {
     this.display = false;
   }
 
-  cancelar(){
+  delete(): void {
+    console.log("Eliminando");
     
   }
 
   ngOnInit(): void {
+    this.formEntrada = this.formBuilder.group({
+      fecha: new FormControl(null, Validators.required),
+      hora_entrada: new FormControl(null, Validators.required),
+      hora_salida: new FormControl(null, Validators.required),
+      persona: new FormControl(null, Validators.required),
+      proyecto: new FormControl(null, Validators.required),
+      festivo: new FormControl(false, Validators.required)
+    });
     this.getAllPersona();
     this.getAllProyecto();
     this.es = {
@@ -227,5 +282,41 @@ export class EntradasComponent implements OnInit {
       today: "Hoy",
       clear: "Borrar"
     };
+    this.cols = [
+      { field: "id", header: "ID" },
+      { field: "persona", subfield: "nombre", header: "Persona" },
+      { field: "proyecto", subfield: "nombre", header: "Proyecto" },
+      { field: "fecha", header: "Fecha" },
+      { field: "hora_entrada", header: "Entrada" },
+      { field: "hora_salida", header: "Salida" }
+      //{ field: "hora_ordinaria", header: "HO" },
+      //{ field: "recargo_nocturno", header: "RN" },
+      //{ field: "hora_extra", header: "HE" },
+      //{ field: "hora_extra_nocturna", header: "HEN" },
+      //{ field: "hora_extra_festiva", header: "HEF" },
+      //{ field: "hora_extra_festiva_nocturna", header: "HEFN" },
+    ];
+    this.items = [
+      // {
+      //   label: "Nuevo",
+      //   icon: "pi pi-fw pi-plus",
+      //   command: () => this.showSaveDialog(false)
+      // },
+      // {
+      //   label: "Editar",
+      //   icon: "pi pi-fw pi-pencil",
+      //   command: () => this.showSaveDialog(true)
+      // },
+      {
+        label: "Eliminar",
+        icon: "pi pi-fw pi-trash",
+        command: () => this.delete()
+      },
+      {
+        label: "Actualizar",
+        icon: "pi pi-fw pi-refresh",
+        command: () => this.getAllRegistros()
+      }
+    ];
   }
 }
